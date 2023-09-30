e97e581e7817d91d8f88d1c88c2a4ba44dc2defd
Henry Hale <henryhale@users.noreply.github.com>
2023-09-30 04:18:05 -0500

    install node and bun

--- setup.sh
+++ setup.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env sh
 
-echo "initial commit";
+echo -e "Initiatilizing...\n";
+
+echo -e "Installing Node.js...\n";
+
+curl https://get.volta.sh | bash;
+
+export VOLTA_HOME="$HOME/.volta";
+
+export PATH="$VOLTA_HOME/bin:$PATH";
+
+volta -v;
+
+volta install node;
+
+node -v;
+
+npm -v;
+
+npm install -g bun;
+
+
+
